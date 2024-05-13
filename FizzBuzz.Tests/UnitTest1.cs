391c7e3ccef643fcf13b04a62a4fe65c5240ca9f
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2024-05-13 17:57:48 +0200

    FirstTest_GenerateList

--- FizzBuzz.Tests/UnitTest1.cs
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace FizzBuzz.Tests
 {
@@ -12,7 +13,28 @@ namespace FizzBuzz.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var generator = new Generator();
+            var numbers = generator.Generate();
+
+            Assert.AreEqual(100, numbers.Count);
+        }
+
+        public class Generator {
+
+            public Generator(){
+
+            }
+
+            public IList<int> Generate()
+            {
+                var numbersList = new List<int>();
+
+                for(int i = 0; i< 100; i++){
+                    numbersList.Add(i+1);
+                }
+
+                return numbersList;
+            }
         }
     }
 }
