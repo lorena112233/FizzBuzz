17d5cb5958b5a3186d31417378a10fd2f20f1205
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2024-05-13 18:20:57 +0200

    Added Tests - Failing

--- FizzBuzz.Tests/GeneratorTests.cs
+++ FizzBuzz.Tests/GeneratorTests.cs
@@ -28,6 +28,42 @@ namespace FizzBuzz.Tests
             Assert.AreEqual("1", numbers[0]);
         }
 
+        [Test]
+        public void GenerateList_ThirdItemShouldBeFizz()
+        {
+            var generator = new Generator();
+            var numbers = generator.Generate();
+
+            Assert.AreEqual("Fizz", numbers[2]);
+        }
+
+        [Test]
+        public void GenerateList_SixthItemShouldBeFizz()
+        {
+            var generator = new Generator();
+            var numbers = generator.Generate();
+
+            Assert.AreEqual("Fizz", numbers[5]);
+        }
+
+        [Test]
+        public void GenerateList_FifthItemShouldBeBuzz()
+        {
+            var generator = new Generator();
+            var numbers = generator.Generate();
+
+            Assert.AreEqual("Buzz", numbers[4]);
+        }
+
+        [Test]
+        public void GenerateList_FifteenthItemShouldBeFizzBuzz()
+        {
+            var generator = new Generator();
+            var numbers = generator.Generate();
+
+            Assert.AreEqual("FizzBuzz", numbers[14]);
+        }
+
 
     }
 }
